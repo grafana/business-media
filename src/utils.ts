ea5208142674d83e487d9864e4d392f2209f1086
Ashley Harrison <ashley.harrison@grafana.com>
2025-10-28 11:11:31 +0000

    remove findField import and inline logic

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,24 @@
 import { DataFrame, Field, FieldMatcherID, fieldMatchers, FieldType, LinkModel, SelectableValue } from '@grafana/data';
-import { findField } from '@volkovlabs/components';
 import { Base64 } from 'js-base64';
 
 import { BASE64_MEDIA_HEADER_REGEX, IMAGE_TYPES_SYMBOLS } from './constants';
 import { MediaFormat, MediaSourceConfig, MediaSourceElement, SupportedFileType } from './types';
+
+const findField = <TValue = unknown>(
+  series: DataFrame[],
+  predicateFn: (field: Field, frame: DataFrame) => boolean
+): Field<TValue> | undefined => {
+  for (let i = 0; i < series.length; i += 1) {
+    const frame = series[i];
+    const field = frame.fields.find((field) => predicateFn(field, frame));
+
+    if (field) {
+      return field;
+    }
+  }
+
+  return undefined;
+};
 
 /**
  * Convert Base64 to Blob
