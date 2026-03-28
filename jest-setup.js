d4a20c74938d06b0991f1e821ebd12b335855f09
Brian Gann <bkgann@gmail.com>
2026-03-27 21:55:00 -0400

    Chore: Upgrade safe dependencies and restore jest-setup mocks
    Bump 15 minor/patch dependencies (babel, prettier, sass, semver, etc.).
    Fix webpack config path in package.json build/dev scripts. Restore
    ResizeObserver and IntersectionObserver mocks in jest-setup.js that
    were removed during the tooling upgrade.

--- jest-setup.js
+++ jest-setup.js
@@ -1,2 +1,20 @@
 // Jest setup provided by Grafana scaffolding
 import './.config/jest-setup';
+
+import ResizeObserver from 'resize-observer-polyfill';
+
+// mock the intersection observer and just say everything is in view
+const mockIntersectionObserver = jest
+  .fn()
+  .mockImplementation((callback) => ({
+    observe: jest.fn().mockImplementation((elem) => {
+      callback([{ target: elem, isIntersecting: true }]);
+    }),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+
+Object.assign(global, {
+  IntersectionObserver: mockIntersectionObserver,
+  ResizeObserver,
+});
